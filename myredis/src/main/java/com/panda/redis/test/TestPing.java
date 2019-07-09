9873c0ba6f46f3be7eb3625ae00f7410466b7b24
likaisheng <1805318928@qq.com>
2019-07-09 16:27:03 +0800

    Initial commit

---
+++ myredis/src/main/java/com/panda/redis/test/TestPing.java
@@ -0,0 +1,15 @@
+package com.panda.redis.test;
+
+import redis.clients.jedis.Jedis;
+
+/**
+ * Created by Administrator on 2019/7/9.
+ */
+public class TestPing {
+    public static void main(String[] args) {
+        //192.168.1.117  注意需关闭Linux的防火墙
+        Jedis jedis = new Jedis("192.168.1.117", 6379);
+        String result = jedis.ping();
+        System.out.println("********ping:" + result);
+    }
+}
