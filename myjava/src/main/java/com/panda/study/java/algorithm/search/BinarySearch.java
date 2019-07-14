157fce4a4f5571a79f7163f9f438abcf04b97ba6
likaisheng <1805318928@qq.com>
2019-07-14 11:48:14 +0800

    插值查找

--- myjava/src/main/java/com/panda/study/java/algorithm/search/BinarySearch.java
+++ myjava/src/main/java/com/panda/study/java/algorithm/search/BinarySearch.java
@@ -9,9 +9,10 @@ import java.util.ArrayList;
 public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {1,8,10,89,1234};
+//        int arr[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
 //        int resIndex = binarySearch(arr, 0, arr.length - 1, 1000);
 //        System.out.println("resIndex=" + resIndex);
-        ArrayList resIndexList = binarySearch2(arr, 0, arr.length - 1, 1000);
+        ArrayList resIndexList = binarySearch2(arr, 0, arr.length - 1, 1);
         System.out.println("resIndexList="+resIndexList);
     }
     //二分查找算法
