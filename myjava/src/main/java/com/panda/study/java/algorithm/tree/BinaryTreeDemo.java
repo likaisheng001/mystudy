060cb10b95aee926cb2f39edc56c72644b9e6faa
likaisheng <1805318928@qq.com>
2019-07-15 14:23:24 +0800

    二叉树的删除

--- myjava/src/main/java/com/panda/study/java/algorithm/tree/BinaryTreeDemo.java
+++ myjava/src/main/java/com/panda/study/java/algorithm/tree/BinaryTreeDemo.java
@@ -20,38 +20,11 @@ public class BinaryTreeDemo {
         node3.setLeft(node5);
         binaryTree.setRoot(root);
 
-        //测试
-        System.out.println("前序遍历");
+        System.out.println("删除前，前序遍历" );
         binaryTree.preOrder();
-        System.out.println("中序遍历");
-        binaryTree.infixOrder();
-        System.out.println("前序遍历");
-        binaryTree.postOrder();
-
-       /* //前序查找的次数：4
-        System.out.println("前序查找方式 - - - ");
-        HeroNode resNode = binaryTree.preOrderSearch(5);
-        if (resNode != null){
-            System.out.printf("找到了，信息为no=%d name=%s",resNode.getNo(),resNode.getName());
-        }else{
-            System.out.printf("没有找到no = %d 的英雄",5);
-        }*/
-        /*//中序查找的次数：3
-        System.out.println("中序查找方式 - - - ");
-        HeroNode resNode = binaryTree.infixOrderSearch(5);
-        if (resNode != null){
-            System.out.printf("找到了，信息为no=%d name=%s",resNode.getNo(),resNode.getName());
-        }else{
-            System.out.printf("没有找到no = %d 的英雄",5);
-        }*/
-        //后序查找的次数：2
-        System.out.println("后序查找方式 - - - ");
-        HeroNode resNode = binaryTree.postOrderSearch(5);
-        if (resNode != null){
-            System.out.printf("找到了，信息为no=%d name=%s",resNode.getNo(),resNode.getName());
-        }else{
-            System.out.printf("没有找到no = %d 的英雄",5);
-        }
+        binaryTree.delNode(3);
+        System.out.println("删除后，前序遍历");
+        binaryTree.preOrder();
     }
 }
 
@@ -116,6 +89,20 @@ class BinaryTree {
             return null;
         }
     }
+    //删除节点
+    public void delNode(int no){
+        if (root != null){
+            //如果只有一个root节点，这里立即判断root是不是就是要删除的节点
+            if (root.getNo() == no){
+                root = null;
+            }else{
+                //递归删除
+                root.delNode(no);
+            }
+        }else {
+            System.out.println("空树，不能删除");
+        }
+    }
 }
 
 //先创建HeroNode节点
@@ -168,6 +155,25 @@ class HeroNode {
 
     public void setRight(HeroNode right) {
         this.right = right;
+    }
+    //递归删除节点
+    //1		如果删除的节点是叶子节点，则删除该节点
+    //2		如果删除的节点是非叶子节点，则删除该子树
+    public void delNode(int no){
+        if (this.left != null && this.left.no == no){
+            this.left = null;
+            return;
+        }
+        if (this.right != null && this.right.no == no){
+            this.right = null;
+            return;
+        }
+        if (this.left != null){
+            this.left.delNode(no);
+        }
+        if (this.right != null){
+            this.right.delNode(no);
+        }
     }
 
     //编写前序遍历的方法
