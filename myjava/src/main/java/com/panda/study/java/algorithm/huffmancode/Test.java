0d109fb4369b4472e98e9556a2e70ea30b61b77b
likaisheng <1805318928@qq.com>
2019-07-17 16:56:31 +0800

    二进制转换

---
+++ myjava/src/main/java/com/panda/study/java/algorithm/huffmancode/Test.java
@@ -0,0 +1,15 @@
+package com.panda.study.java.algorithm.huffmancode;
+
+/**
+ * Created by Administrator on 2019/7/17.
+ */
+public class Test {
+    public static void main(String[] args) {
+        //二进制转换
+        //byte[推导10101000 => 10101000-1 => 10100111(反码) => 11011000 => -88]
+        String strByte = "10101000";
+//        System.out.println((byte)Integer.parseInt(strByte,2)); -88
+        //168
+//        System.out.println(Integer.parseInt(strByte,2));
+    }
+}
