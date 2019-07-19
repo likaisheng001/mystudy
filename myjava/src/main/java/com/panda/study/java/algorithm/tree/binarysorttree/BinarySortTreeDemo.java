911b289fa3946a8891ce4700644afffab08047d5
likaisheng <1805318928@qq.com>
2019-07-19 11:34:08 +0800

    二叉排序树-删除叶子节点代码

--- myjava/src/main/java/com/panda/study/java/algorithm/tree/binarysorttree/BinarySortTreeDemo.java
+++ myjava/src/main/java/com/panda/study/java/algorithm/tree/binarysorttree/BinarySortTreeDemo.java
@@ -5,19 +5,72 @@ package com.panda.study.java.algorithm.tree.binarysorttree;
  */
 public class BinarySortTreeDemo {
     public static void main(String[] args) {
-        int arr[] = {7, 3, 10, 12, 5, 1, 9};
+        int arr[] = {7, 3, 10, 12, 5, 1, 9,2};
         BinarySortTree binarySortTree = new BinarySortTree();
         //循环的添加结点到二叉排序树
         for (int i = 0; i < arr.length; i++) {
             binarySortTree.add(new Node(arr[i]));
         }
         //中序遍历二叉排序树
+        System.out.println("中序遍历二叉排序树");
+        binarySortTree.infixOrder();
+        //测试一下删除叶子节点
+//        binarySortTree.delNode(2);
+        binarySortTree.delNode(12);
+        System.out.println("删除节点后...");
         binarySortTree.infixOrder();
     }
 }
 //创建二叉排序树
 class BinarySortTree{
     private Node root;
+    //查找要删除的节点
+    public Node search(int value){
+        if (root == null){
+            return null;
+        }else{
+            return root.search(value);
+        }
+    }
+    //查找父结点
+    public Node searchParent(int value){
+        if (root == null){
+            return null;
+        }else {
+            return root.searchParent(value);
+        }
+    }
+    //删除节点
+    public void delNode(int value){
+        if (root == null){
+            return;
+        }else {
+            //1.需求先去找到要删除的结点  targetNode
+            Node targetNode = search(value);
+            //如果没有找到要删除的结点
+            if (targetNode == null){
+                return;
+            }
+            //如果我们发现当前这颗二叉排序树只有一个节点
+            if (root.left == null && root.right == null){
+                root = null;
+                return;
+            }
+            //去找到targetNode的父结点
+            Node parent = searchParent(value);
+            //如果要删除的是叶子节点
+            if (targetNode.left == null && targetNode.right == null){
+                //判断targetNode是父结点的左子节点还是右子节点
+                if (parent.left != null && parent.left.value == targetNode.value){
+                    parent.left = null;
+                }else if (parent.right != null && parent.right.value == targetNode.value){
+                    parent.right = null;
+                }
+            }
+            //2.找到targetNode 的 父结点 parent
+            Node node = searchParent(value);
+        }
+    }
     //添加节点的方法
     public void add(Node node){
         if (root == null){
@@ -37,6 +90,7 @@ class BinarySortTree{
 class Node{
     int value;
     Node left;
+    Node right;
 
     @Override
     public String toString() {
@@ -45,8 +99,49 @@ class Node{
                 '}';
     }
 
-    Node right;
-
+    //查找要删除的节点
+    /**
+     *
+     * @param value 希望要删除的结点的值
+     * @return 如果找到返回该结点，否则返回null
+     */
+    public Node search(int value){
+        if (value == this.value){//找到该结点
+            return this;
+        }else if (value < this.value){//如果查找的值小于当前结点，向左子树递归查找
+            //如果左子节点为空
+            if (this.left == null){
+                return null;
+            }
+            return this.left.search(value);
+        }else{
+            if (this.right == null){
+                return null;
+            }
+            return this.right.search(value);
+        }
+    }
+    //查找要删除结点的父节点
+    /**
+     *
+     * @param value 要找到的节点的值
+     * @return 返回的是要删除的节点的父节点，如果没有就返回null
+     */
+    public Node searchParent(int value){
+        if ((this.left != null && this.left.value == value) ||
+                (this.right != null && this.right.value == value)){
+            return this;
+        }else {
+            //如果查找的值小于当前结点的值，并且当前结点的左子节点不为空
+            if (value < this.value && this.left != null){
+                return this.left.searchParent(value);//向左子树递归查找
+            }else if (value >= this.value && this.right != null){
+                return this.right.searchParent(value);//向右子树递归查找
+            }else {
+                return null;//没有找到父节点
+            }
+        }
+    }
     public Node(int value) {
         this.value = value;
     }
