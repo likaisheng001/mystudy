740cfb9085cd931ffb50a934f9beda65c630cfe5
likaisheng <1805318928@qq.com>
2019-07-28 14:51:09 +0800

    原型模式

---
+++ myspring/src/main/java/com/panda/study/prototype/improve/Client.java
@@ -0,0 +1,20 @@
+package com.panda.study.prototype.improve;
+
+/**
+ * Created by Administrator on 2019/7/28.
+ */
+public class Client {
+    public static void main(String[] args) {
+        Sheep sheep = new Sheep("tom", 1, "白色");
+        Sheep sheep2 = (Sheep)sheep.clone();
+        Sheep sheep3 = (Sheep)sheep.clone();
+        Sheep sheep4 = (Sheep)sheep.clone();
+        Sheep sheep5 = (Sheep)sheep.clone();
+
+        System.out.println("sheep="+sheep);
+        System.out.println("sheep2="+sheep2);
+        System.out.println("sheep3="+sheep3);
+        System.out.println("sheep4="+sheep4);
+        System.out.println("sheep5="+sheep5);
+    }
+}
